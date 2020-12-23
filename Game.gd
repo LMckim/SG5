b548c4ca815fc40b64f1c36a6d071a26e456ce1b
Luke Mckim <lmckim.dev@gmail.com>
2020-12-22 22:06:22 -0500

    doors working, interior up

--- Game.gd
+++ Game.gd
@@ -6,6 +6,9 @@ extends Node2D
 # var b = "text"
 
 export var zoom_speed = 0.05
+export var zoom_in_limit = 0.5
+export var zoom_out_limit = 1.5
+var current_zoom = 1
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -15,10 +18,15 @@ func _ready():
 
 func _input(event):
 	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_WHEEL_UP:
-			$Player/Cam.zoom += Vector2(zoom_speed, zoom_speed)
-		elif event.button_index == BUTTON_WHEEL_DOWN:
+		if event.button_index == BUTTON_WHEEL_UP and current_zoom > zoom_in_limit:
 			$Player/Cam.zoom -= Vector2(zoom_speed, zoom_speed)
+			current_zoom -= 0.1
+		elif event.button_index == BUTTON_WHEEL_DOWN and current_zoom < zoom_out_limit:
+			$Player/Cam.zoom += Vector2(zoom_speed, zoom_speed)
+			current_zoom += 0.1
 			
 			
+func _process(delta):
+#	$ship.position.y -= 5 * delta
+	pass
 
---
+++ scenes/ship/components/door.gd
@@ -0,0 +1,49 @@
+extends Area2D
+
+export var slide_speed = 3
+export var slide_max = 100
+export var locked = false
+
+enum state {OPEN, OPENING, CLOSED, CLOSING}
+
+var sliding = false
+var slide_dist = 0
+var occupied = false
+var curr_state = state.CLOSED
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var move_dist = slide_speed * delta
+	if move_dist + slide_dist > slide_max:
+		move_dist = slide_max - slide_dist
+	
+	if curr_state == state.OPENING:
+		$Door_Body.position.y -= move_dist	
+		slide_dist += move_dist
+	if curr_state == state.CLOSING:
+		$Door_Body.position.y += move_dist	
+		slide_dist += move_dist
+		
+	if slide_dist == slide_max:
+		curr_state = state.OPEN if curr_state == state.OPENING else state.CLOSED
+		slide_dist = 0
+		
+	if not occupied and curr_state == state.OPEN:
+		curr_state = state.CLOSING
+			
+			
+func _on_Sliding_Door_body_entered(body):
+	if body.name != "Player": return
+	print("Entered")
+	
+	occupied = true
+	if curr_state == state.CLOSED or curr_state == state.CLOSING:
+		curr_state = state.OPENING
+
+func _on_Sliding_Door_body_exited(body):
+	if body.name != "Player": return
+	print("Exited")
+	
+	occupied = false
+	if curr_state == state.OPEN or curr_state == state.CLOSED:
+		curr_state = state.CLOSING
